eb1cdaff912a3dbb89d35072b4a2f9fc0253207d
Colin McCormack <80981213+NorthernSantan@users.noreply.github.com>
2021-10-27 20:00:32 -0400

    Update main.c

--- AO1FractionProgram/main.c
+++ AO1FractionProgram/main.c
@@ -1,36 +1,3 @@
-/*
-
-  A01 Fraction Program
-
-  Program Structure:
-
-    - This file:
-      Runs fnctions from various header files.
-    
-    - IO.h
-      Header file containing IO functions, 
-      Interface between User and Program 
-
-    - Operations.h
-      Header file containing all functions created for specific operations,
-      Including Addition, Subtraction, Multiplication, and so on...
-      All those functions are abstracted away.
-      The only visible functions are decideWhatToDo, and doWhatIsToBeDone.
-      decideWhatToDo will return a function pointer that doWhatIsToBeDone runs.
-
-    - DataBase.h
-      Contains MultiDimensional Arrays that can be used to store data.
-      Syntax Sugar: Just include this header file to access these arrays instead
-      of passing them by reference each time.
-
-
-  @author Abdul Mannan Syed, asyed24@ocdsb.ca
-  @author Collin McMcormack, cmcco3@ocdsb.ca
-
-  @date  10/26/2021
-
-*/
-
 // Header files
 #include "IO.h"
 #include "Operations.h"
